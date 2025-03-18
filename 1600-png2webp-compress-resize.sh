3b204450211270f5e912c89a75b94f124fa058a7
Cesar Kyle Casil <ccasil@users.noreply.github.com>
2025-03-18 16:37:28 -0700

    update

--- 1600-png2webp-compress-resize.sh
+++ 1600-png2webp-compress-resize.sh
@@ -6,12 +6,12 @@ for i in $(ls | egrep -i '^1600\-(.*)+\_[0-9][0-9]\.png$'); do
     j=${i:5}
     FNAME=$(echo "$j" | sed -e 's/\..*$//')
     INAME=${FNAME:0:${#FNAME}-3}
-    convert "$i" -resize 1024 base-"$j"
-    convert base-"$j" -trim +repage 1024-"$j"
-    convert "$i" -define filter:blur=0.4 -filter Gaussian -resize 800 -flatten -quality 90% 800-"$FNAME".webp
-    convert "$i" -define filter:blur=0.4 -filter Gaussian -resize 480 -flatten -quality 90% 480-"$FNAME".webp
-    convert "$i" -resize 180 180-"$j"
-    convert 180-"$j" -gravity center +repage -extent 200x200 +repage -flatten -quality 90% "$FNAME".webp
+    magick "$i" -resize 1024 base-"$j"
+    magick base-"$j" -trim +repage 1024-"$j"
+    magick "$i" -define filter:blur=0.3 -filter Lanczos -resize 800 -flatten -quality 95% 800-"$FNAME".webp
+    magick "$i" -define filter:blur=0.3 -filter Lanczos -resize 480 -flatten -quality 95% 480-"$FNAME".webp
+    magick "$i" -resize 180 180-"$j"
+    magick 180-"$j" -gravity center +repage -extent 200x200 +repage -flatten -quality 95% "$FNAME".webp
     # mv "$i" /mnt/backup/product_img_1600/;
     rm -f  base-"$j";
     rm -f 180-"$j";
@@ -22,4 +22,3 @@ for i in $(ls | egrep -i '^1600\-(.*)+\_[0-9][0-9]\.png$'); do
     pngquant 1024-"$j" --ext .webp --force;
 done;
 IFS=$SAVEIFS;
-
